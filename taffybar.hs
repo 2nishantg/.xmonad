6b906a264ae43a92f53c0c8b46d22c52fdc787f7
Nishant <nishant@cse.iitk.ac.in>
2015-06-24 21:13:27 +0530

    Add: Taffybar Config.

---
+++ taffybar.hs
@@ -0,0 +1,58 @@
+import System.Taffybar
+
+import System.Taffybar.Battery
+import System.Taffybar.Systray
+import System.Taffybar.TaffyPager
+import System.Taffybar.SimpleClock
+import System.Taffybar.FreedesktopNotifications
+import System.Taffybar.Weather
+import System.Taffybar.MPRIS
+import System.Taffybar.Pager
+import System.Taffybar.Widgets.PollingBar
+import System.Taffybar.Widgets.PollingGraph
+
+import System.Taffybar.WorkspaceSwitcher
+import System.Information.Memory
+import System.Information.CPU
+
+memCallback = do
+  mi <- parseMeminfo
+  return [memoryUsedRatio mi]
+
+cpuCallback = do
+  (userLoad, systemLoad, totalLoad) <- cpuLoad
+  return [totalLoad, systemLoad]
+
+main = do
+  let memCfg = defaultGraphConfig { graphDataColors = [(1, 0, 0, 1)]
+                                  , graphLabel = Just "mem"
+                                  }
+      cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1)
+                                                      , (1, 0, 1, 0.5)
+                                                      ]
+                                  , graphLabel = Just "cpu"
+                                  }
+  let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
+      pager = taffyPagerNew defaultPagerConfig
+                  { activeWindow     = colorize "#00ff00" "" . escape . shorten 40
+                  , activeLayout     = escape
+                  , activeWorkspace  = colorize "#ffca00" "" . escape
+                  , hiddenWorkspace  = colorize "#aaaaaa" "" . escape
+                  , emptyWorkspace   = colorize "#666666" "" . escape
+                  , visibleWorkspace = colorize "#a88500" "" . escape
+                  , urgentWorkspace  = colorize "red" "yellow" . escape
+                  , widgetSep        = " : "
+                  }
+      note = notifyAreaNew defaultNotificationConfig
+      wea = weatherNew (defaultWeatherConfig "KMSN") 10
+      mpris = mprisNew defaultMPRISConfig
+      mem = pollingGraphNew memCfg 1 memCallback
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      tray = systrayNew
+      battery = batteryBarNew  defaultBatteryConfig 25
+
+  defaultTaffybar defaultTaffybarConfig { startWidgets = [ pager, note ]
+                                        , endWidgets = [ tray, wea, clock,battery ,mem, cpu, mpris ]
+                                        , monitorNumber = 1
+                                        , barPosition = Bottom
+                                        }
