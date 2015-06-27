fa767719f937b8ec3c2c43f53e4eae213810473c
Nishant <nishant@cse.iitk.ac.in>
2015-06-27 14:17:57 +0530

    Change theme to monochrome

--- taffybar.hs
+++ taffybar.hs
@@ -5,39 +5,42 @@ import System.Taffybar.Systray
 import System.Taffybar.TaffyPager
 import System.Taffybar.SimpleClock
 import System.Taffybar.FreedesktopNotifications
-import System.Taffybar.Weather
+import System.Taffybar.Weather()
 import System.Taffybar.MPRIS
 import System.Taffybar.Pager
-import System.Taffybar.Widgets.PollingBar
+import System.Taffybar.Widgets.PollingBar()
 import System.Taffybar.Widgets.PollingGraph
-import System.Taffybar.Widgets.PollingLabel
+import System.Taffybar.Widgets.PollingLabel()
 import System.Taffybar.NetMonitor
-import System.Taffybar.WorkspaceSwitcher
+import System.Taffybar.WorkspaceSwitcher()
 import System.Information.Memory
 import System.Information.CPU
 
+memCallback :: IO [Double]
 memCallback = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
 
+cpuCallback :: IO [Double]
 cpuCallback = do
-  (userLoad, systemLoad, totalLoad) <- cpuLoad
+  (_, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
+main :: IO ()
 main = do
-  let memCfg = defaultGraphConfig { graphDataColors = [(1, 0, 0, 1)]
-                                  , graphLabel = Just "mem"
+  let memCfg = defaultGraphConfig { graphDataColors = [(1, 1, 1, 1)]
+                                  , graphLabel = Just "Mem"
                                   }
-      cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1)
+      cpuCfg = defaultGraphConfig { graphDataColors = [ (1, 1, 1, 1)
                                                       , (1, 0, 1, 0.5)
                                                       ]
-                                  , graphLabel = Just "cpu"
+                                  , graphLabel = Just "Cpu"
                                   }
-  let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
+  let clock = textClockNew Nothing "<span fgcolor='dark grey'>%a %b %_d %H:%M</span>" 1
       pager = taffyPagerNew defaultPagerConfig
-                  { activeWindow     = colorize "#00ff00" "" . escape . shorten 40
+                  { activeWindow     = colorize "#93a1a1" "" . escape . shorten 40
                   , activeLayout     = escape
-                  , activeWorkspace  = colorize "#ffca00" "" . escape
+                  , activeWorkspace  = colorize "#ffffff" "" . escape
                   , hiddenWorkspace  = colorize "#aaaaaa" "" . escape
                   , emptyWorkspace   = colorize "#666666" "" . escape
                   , visibleWorkspace = colorize "#a88500" "" . escape
